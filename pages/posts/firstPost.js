19bd12b6e4968688932220715ce2937c634b615a
yumaon <110388364+yumaon@users.noreply.github.com>
2025-12-29 17:58:37 +0900

    Linkコンポーネントでページ遷移

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 export default function firstPost() {
   return (
     <div>
       <h1>最初の投稿</h1>
-      <h2>ホームへ戻る</h2>
+      <h2><Link href="/">ホームへ戻る</Link></h2>
     </div>
   );
 }

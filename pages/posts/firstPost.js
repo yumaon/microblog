8d12a148bcd8c361e452c683e980e3dbf1d7de13
yumaon <110388364+yumaon@users.noreply.github.com>
2025-12-29 18:05:12 +0900

    Headコンポーネントでメタデータを追記

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,7 +1,12 @@
 import Link from 'next/link'
-export default function firstPost() {
+import Head from 'next/head'
+export default function FirstPost() {
   return (
     <div>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
+      
       <h1>最初の投稿</h1>
       <h2><Link href="/">ホームへ戻る</Link></h2>
     </div>
